54b81a479d29e11935ab8d21b77ba5c1cabf2247
Nimira43 <n.rabone72@aol.com>
2024-10-31 21:09:14 +0000

    Brought Billy in!

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,22 @@
+'use client'
+
+import Image from 'next/image'
+
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='h-screen grid place-items-center'>
+      <div>
+        <div>
+          <div>
+            <Image 
+              src={'/bee.png'} 
+              alt='NimiraTech Billy Bee' 
+              width={400} 
+              height={400} 
+              priority={true} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
