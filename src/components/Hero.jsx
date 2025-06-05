f7f3795a0b4eab80d4023aceca42648aa12d4c50
Nimira43 <n.rabone72@aol.com>
2025-06-05 02:54:27 +0100

    Setting up basic hero page.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
 'use client'
 
+import Image from 'next/image'
+
 const Hero = () => {
   return (
-    <div>
-      hero
+    <div className='h-screen grid place-items-center'>
+      <div>
+        <div>
+          <div>
+            <Image 
+              src='/images/dragon-1.png'
+              alt='Dragon image'
+              width={400}
+              height={400}
+              priority={true}
+            />
+            <span>1997</span>
+          </div>
+          <h1>Dragon Interactive</h1>
+          <p>Creating New Horizons</p>
+        </div>
+        <div>
+          <a href='#'>Icon</a>
+        </div>
+        <a href='#'>View Games</a>
+      </div>
     </div>
   )
 }
