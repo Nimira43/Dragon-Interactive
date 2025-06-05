3b08e8181e1a1f7f3c94ee668af0fcbaf4b45003
Nimira43 <n.rabone72@aol.com>
2025-06-05 03:20:00 +0100

    Styling the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,23 +7,29 @@ const Hero = () => {
     <div className='h-screen grid place-items-center'>
       <div>
         <div className='flex flex-col items-center justify-center gap-y-3 font-normal'>
-          <div>
+          <div className='flex items-center justify-center'>
             <Image 
               src='/images/dragon-1.png'
               alt='Dragon image'
               width={400}
               height={400}
               priority={true}
+              className='h-auto w-[150px]'
             />
-            <span>1997</span>
+            {/* <span className='absolute text-3xl font-medium text-main'>1997</span> */}
           </div>
-          <h1>Dragon Interactive</h1>
-          <p>Creating New Horizons</p>
+          <h1 className='text-center text-3xl font-medium tracking-wider logo'>Dragon Interactive</h1>
+          <p className='text-sm logo '>Creating New Horizons</p>
         </div>
         <div>
           <a href='#'>Icon</a>
         </div>
-        <a href='#'>View Games</a>
+        <a 
+          href='#'
+          className='mx-auto mt-7 block w-max px-3 py-1 border border-dark uppercase cursor-pointer hover:border-main hover:text-main transition-all'
+        >
+          View Games
+        </a>
       </div>
     </div>
   )
