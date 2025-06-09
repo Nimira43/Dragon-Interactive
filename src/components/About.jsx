50a8bdaea9d0bb28877fc958dbcda9301d83a7a2
Nimira43 <n.rabone72@aol.com>
2025-06-09 12:57:10 +0100

    Making various tweaks to the About and Heading code.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,10 @@ import Heading from './sections/Heading'
 import Accolades from './sections/Accolades'
 import { aboutAwards } from './helpers/variables'
 import { IoDownloadOutline } from 'react-icons/io5'
-import { IoMdArrowDropleft } from 'react-icons/io'
 
 const About = () => {
   return (
-    <div className='min-h-screen px-96 flex flex-col items-center justify-center'>
+    <div className='min-h-screen px-48 flex flex-col items-center justify-center'>
       <Heading text={'About Us'}/>
       <div className='w-full flex items-center justify-between md:justify-center'>
         <Image
@@ -19,14 +18,19 @@ const About = () => {
           width={400}
           height={400}
         />
-        <div className=' relative max-w-[800px] p-5 text-justify'>
-          <span className='absolute -left-5 top-20 scale-2.5 md:hidden'>
-            <IoMdArrowDropleft />
-          </span>
-          <p>Dragon Interactive is a pioneering game studio redefining interactive storytelling. With titles like Qine and Gemini, we merge science fiction, philosophy, and cutting-edge technology into compelling games. Our upcoming project, VNR, promises a gripping post-apocalyptic mystery, solidifying our reputation for crafting thought-provoking, immersive worlds that challenge players’ perceptions and intellect.</p>
-          <a href="/images/The-Evolution-of-Dragon-Interactive.pdf" download=''>
-          <span>Download Gamer Review</span>
-          <span>Download Icon</span>
+        <div className='max-w-[800px] p-5 text-center'>
+          <p className='text-lg lg:text-[16px] sm:text-[14px]'>Dragon Interactive is a pioneering game studio redefining interactive storytelling. With titles like Qine and Gemini, we merge science fiction, philosophy, and cutting-edge technology into compelling games. Our upcoming project, VNR, promises a gripping post-apocalyptic mystery, solidifying our reputation for crafting thought-provoking, immersive worlds that challenge players’ perceptions and intellect.</p>
+          <a 
+            href="/images/The-Evolution-of-Dragon-Interactive.pdf" 
+            download=''
+            className='w-max flex items-center gap-x-2 mt-6 hover:text-main transition-colors'
+          >
+            <span>Download Gamer Review</span>
+            <span
+             className='text-xl'
+            >
+              <IoDownloadOutline />
+            </span>
           </a>
         </div>
       </div>
--- src/components/sections/Heading.jsx
+++ src/components/sections/Heading.jsx
@@ -2,7 +2,7 @@
 
 const Heading = ({ text }) => {
   return (
-    <h1 className='text-3xl sm:text-2xl mb-14 self-start logo'>
+    <h1 className='text-3xl sm:text-2xl mb-14 text-center logo'>
       { text }
     </h1>
   )
