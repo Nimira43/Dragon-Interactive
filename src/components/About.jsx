3ae5f460dc8c8225ba1bfd0fbe4f3a0290db5cf6
Nimira43 <n.rabone72@aol.com>
2025-06-09 10:57:00 +0100

    Placeholder markup. Plus pdf about Dragon Interactive.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,6 +14,11 @@ const About = () => {
           width={400}
           height={400}
         />
+        <div>
+          <span>Arrow Left</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ducimus quaerat ad laudantium atque consequatur nulla eos explicabo provident magni voluptates ipsam eveniet maxime quia dolore deleniti. Porro, cum ullam?</p>
+          <a href="/newsletter.pdf"></a>
+        </div>
       </div>
     </div>
   )
