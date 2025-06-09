2cbb7dd9fde6d6585d12d31be04ae7bb079f6d91
Nimira43 <n.rabone72@aol.com>
2025-06-09 12:36:08 +0100

    Styling left icon.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,8 +19,10 @@ const About = () => {
           width={400}
           height={400}
         />
-        <div>
-          <span>Arrow Left</span>
+        <div className=' relative max-w-[800px] p-5 text-justify'>
+          <span className='absolute -left-5 top-20 scale-2.5 md:hidden'>
+            <IoMdArrowDropleft />
+          </span>
           <p>Dragon Interactive is a pioneering game studio redefining interactive storytelling. With titles like Qine and Gemini, we merge science fiction, philosophy, and cutting-edge technology into compelling games. Our upcoming project, VNR, promises a gripping post-apocalyptic mystery, solidifying our reputation for crafting thought-provoking, immersive worlds that challenge players’ perceptions and intellect.</p>
           <a href="/images/The-Evolution-of-Dragon-Interactive.pdf" download=''>
           <span>Download Gamer Review</span>
