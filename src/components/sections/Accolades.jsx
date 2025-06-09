8f4c1f69f951b18e31c5dbc6a886ffcff7466580
Nimira43 <n.rabone72@aol.com>
2025-06-09 15:10:41 +0100

    Styling to mark up in Accolade

--- src/components/sections/Accolades.jsx
+++ src/components/sections/Accolades.jsx
@@ -1,10 +1,10 @@
 const Accolades = () => {
   return (
-    <div>
-      <span>Icon</span>
-      <h1>
-        <span>10</span>
-        <span>Game of the Year Awards</span>
+    <div className='flex items-center gap-x-3'>
+      <span className='text-4xl lg:text-2xl'>Icons</span>
+      <h1 className='flex flex-col gap-y-2'>
+        <span className='text-2xl lg:text-xl'>10</span>
+        <span className='text-sm'>Game of the Year Awards</span>
       </h1>     
     </div>
   )
