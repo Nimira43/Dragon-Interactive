2198fdf2c84848c741cf18795f37c9f47c5f2ef6
Nimira43 <n.rabone72@aol.com>
2025-06-09 11:13:24 +0100

    Download link.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,10 @@ const About = () => {
         <div>
           <span>Arrow Left</span>
           <p>Dragon Interactive is a pioneering game studio redefining interactive storytelling. With titles like Qine and Gemini, we merge science fiction, philosophy, and cutting-edge technology into compelling games. Our upcoming project, VNR, promises a gripping post-apocalyptic mystery, solidifying our reputation for crafting thought-provoking, immersive worlds that challenge players’ perceptions and intellect.</p>
-          <a href="/images/The-Evolution-of-Dragon-Interactive.pdf"></a>
+          <a href="/images/The-Evolution-of-Dragon-Interactive.pdf" download=''>
+          <span>Download Gamer Review</span>
+          <span>Download Icon</span>
+          </a>
         </div>
       </div>
     </div>
