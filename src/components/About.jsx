3241162eb5c8eff493be8eece3d30d13be7e81b8
Nimira43 <n.rabone72@aol.com>
2025-06-09 11:06:49 +0100

    Created About content. Also link to a pdf about Dragon Interactive.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
         />
         <div>
           <span>Arrow Left</span>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ducimus quaerat ad laudantium atque consequatur nulla eos explicabo provident magni voluptates ipsam eveniet maxime quia dolore deleniti. Porro, cum ullam?</p>
-          <a href="/newsletter.pdf"></a>
+          <p>Dragon Interactive is a pioneering game studio redefining interactive storytelling. With titles like Qine and Gemini, we merge science fiction, philosophy, and cutting-edge technology into compelling games. Our upcoming project, VNR, promises a gripping post-apocalyptic mystery, solidifying our reputation for crafting thought-provoking, immersive worlds that challenge players’ perceptions and intellect.</p>
+          <a href="/images/The-Evolution-of-Dragon-Interactive.pdf"></a>
         </div>
       </div>
     </div>
