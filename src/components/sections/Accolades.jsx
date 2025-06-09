a297eaef64bcc36ad7969f1e113df07686ecff8e
Nimira43 <n.rabone72@aol.com>
2025-06-09 15:18:56 +0100

    Setting up count method

--- src/components/sections/Accolades.jsx
+++ src/components/sections/Accolades.jsx
@@ -1,6 +1,21 @@
 import { motion, useMotionValue } from 'framer-motion'
 
-const Accolades = () => {
+const Accolades = ({ title, amount, children }) => {
+  const number = useMotionValue(0)
+  const count = (amount) => {
+    let i = 0
+    const updateCount = () => {
+      let timeOut
+      if (i <= amount) {
+        number.set(i++)
+        timeOut = setTimeout(updateCount, 0)
+      } else {
+        clearTimeout(timeOut)
+      }
+    }
+    updateCount()
+  }
+  
   return (
     <div className='flex items-center gap-x-3'>
       <span className='text-4xl lg:text-2xl'>Icons</span>
