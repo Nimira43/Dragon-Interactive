e2c3680830cb3b827de08c2143ad577774f58d20
Nimira43 <n.rabone72@aol.com>
2025-06-28 16:01:15 +0100

    Created mark up for first timeline entry.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import About from '@/components/About'
-
 import Hero from '@/components/Hero'
+import TimeLine from '@/components/TimeLine'
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <TimeLine />
     </div>
   )
 }
--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -8,15 +8,30 @@ const TimeLine = () => {
     <div>
       <Heading 
         text={'Placeholder'}
-      >
-        <Image 
-          src={''}
-          alt='Timeline Dragon Image'
+      />
+      <Image 
+          src={'/images/timeline.png'}
+          alt={'Timeline Dragon Image'}
           width={400}
           height={400}      
-        />
-
-      </Heading>
+      />
+      <div>
+        <div>
+          <h1>Lorem ipsum dolor sit.</h1>
+          <p>
+            <span>Lorem, ipsum dolor.</span>
+            <span>Lorem, ipsum dolor.</span>
+          </p>
+          <div>
+            <span>Lorem</span>
+            <ul>
+              <li>Icon</li>
+            </ul>
+          </div>
+          <span>Date</span>
+        </div>
+        <div>Timeline</div>
+      </div>
     </div>
   )
 }
