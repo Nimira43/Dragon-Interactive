daf6567e818be6a0b73f90585a30ebe5d777e8b8
Nimira43 <n.rabone72@aol.com>
2025-06-28 15:37:52 +0100

    Added new timeline image

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,7 +1,22 @@
+'use client'
+
+import Image from 'next/image'
+import Heading from './sections/Heading'
+
 const TimeLine = () => {
   return (
     <div>
-      
+      <Heading 
+        text={'Placeholder'}
+      >
+        <Image 
+          src={''}
+          alt='Timeline Dragon Image'
+          width={400}
+          height={400}      
+        />
+
+      </Heading>
     </div>
   )
 }
