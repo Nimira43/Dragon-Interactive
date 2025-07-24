e4b0e76b148adaa22ece121b0a5227e7c3324090
Nimira43 <n.rabone72@aol.com>
2025-07-24 01:07:13 +0100

    Reviewed markup

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -6,30 +6,30 @@ import Heading from './sections/Heading'
 const TimeLine = () => {
   return (
     <div className='py-20 px-96 relative'>
-      <Heading 
-        text={'Placeholder'}
-      />
       <Image 
           src={'/images/timeline.png'}
           alt={'Timeline Dragon Image'}
           width={400}
           height={400} 
-          className='absolute -top-4 opacity-70 lg:hidden k'     
+          className='absolute -top-4 right-96 opacity-70 lg:hidden'     
       />
-      <div className='w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10' >
-        <div>
-          <h1>Lorem ipsum dolor sit.</h1>
-          <p>
-            <span>Lorem, ipsum dolor.</span>
-            <span>Lorem, ipsum dolor.</span>
-          </p>
+      <div className='w-full h-full flex flex-col items-center justify-center gap-y-10 lg:gap-y-20 py-10'>
+        <div className='w-[600px] xl:w-[480px] sm:w-full px-12 sm:px-0 relative -left-[300px] bg-yellow-200'>
           <div>
-            <span>Lorem</span>
-            <ul>
-              <li>Icon</li>
-            </ul>
+            <h1>Our Approach</h1>
+            <p>
+              <span>History</span>
+              <span>History</span>
+            </p>
+            <div>
+              <span>Experience</span>
+              <ul>
+                <li>Experience</li>
+              </ul>
+            </div>
+            <span>Icon</span>
           </div>
-          <span>Date</span>
+          <div>2025</div>  
         </div>
         <div>Timeline</div>
       </div>
